c48fa98e8f6658d5a2f955b9a091d6317ed54032
Gaurav Chand <gc.27@outlook.com>
2020-06-06 16:11:54 +0530

    Updates

---
+++ precache-manifest.ebc7b251122d3dc999ce1f0c69a658df.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "157bd2b182885cd37d141173e850ed43",
+    "url": "/justMusic/index.html"
+  },
+  {
+    "revision": "d53576be8c2568ebd9e5",
+    "url": "/justMusic/static/css/main.8661fd60.chunk.css"
+  },
+  {
+    "revision": "521b608cbb61b8f0bdfb",
+    "url": "/justMusic/static/js/2.d492c581.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/justMusic/static/js/2.d492c581.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d53576be8c2568ebd9e5",
+    "url": "/justMusic/static/js/main.5f35463c.chunk.js"
+  },
+  {
+    "revision": "bd72a3b0bac2af2c22fb",
+    "url": "/justMusic/static/js/runtime-main.4cf43b2b.js"
+  },
+  {
+    "revision": "03bb12eaf60008c20e366eb46fbcf036",
+    "url": "/justMusic/static/media/bg.03bb12ea.jpg"
+  },
+  {
+    "revision": "454d3610cce0881524235968dc27ea36",
+    "url": "/justMusic/static/media/bg1.454d3610.jpg"
+  },
+  {
+    "revision": "9f3d3830a63f21f4e8f58fee73c1d9bf",
+    "url": "/justMusic/static/media/bg2.9f3d3830.jpg"
+  }
+]);
